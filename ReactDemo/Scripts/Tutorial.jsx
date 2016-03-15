1f546579475e56ac8bd0e093b61fc445792d55ff
Yuriy Dolzhikov <urise@i.ua>
2016-03-15 22:31:21 +0200

    Initial Commit

---
+++ ReactDemo/Scripts/Tutorial.jsx
@@ -0,0 +1,52 @@
+﻿var Comment = React.createClass({
+  render: function() {
+    var converter = new Showdown.converter();
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">{this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+          <div className="commentForm">
+            Hello, world! I am a CommentForm.
+          </div>
+      );
+    }
+});
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  React.createElement(CommentBox, null),
+  document.getElementById('content')
+);
